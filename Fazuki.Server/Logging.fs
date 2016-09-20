c693030ffd5afe17129dbb495b502402ff5a842e
Michael Shanks <mjashanks@hotmail.com>
2016-09-20 23:42:47 +0100

    fix loggers for new pipeline

--- Fazuki.Server/Logging.fs
+++ Fazuki.Server/Logging.fs
@@ -16,24 +16,24 @@ module Logging =
 
     let GetLoggingFilters logger = 
     
-        let LogDebug message = logger Debug message    
-        let LogError message = logger Error message    
+        let LogDebug id message = logger Debug message    
+        let LogError id message = logger Error message    
 
         let ReceiveLogger (result:ReceiveResult) = 
-            match result with
-            | Success(r) -> LogDebug (sprintf "Received Success: %i" r.EncodedRequest.Length)
+            match result.StepResult with
+            | Success(r) -> LogDebug result.Id (sprintf "Received Success: %i" r.EncodedRequest.Length)
             | Failed(e) -> 
                 match e with
-                | ReceiveError(e) -> LogError (e.ToString())
+                | ReceiveError(e) -> LogError result.Id (e.ToString())
                 | _ -> ()
             result
 
         let DecodeLogger (result:DecodeResult) = 
-            match result with
-            | Success(r) -> LogDebug (sprintf "Decode Success: %s" r.DecodedRequest)
+            match result.StepResult with
+            | Success(r) -> LogDebug result.Id (sprintf "Decode Success: %s" r.DecodedRequest)
             | Failed(e) -> 
                 match e with
-                | DecodeError(e) -> LogError (e.ToString())
+                | DecodeError(e) -> LogError result.Id (e.ToString())
                 | _ -> ()
             result
 
